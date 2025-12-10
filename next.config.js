7e07ce20c4d7edad75887e5134059ae9e7a0ef76
AdamFirman8124 <adamfirman078@gmail.com>
2025-12-10 10:54:41 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {}, // aktifkan Turbopack tanpa konfigurasi khusus
+  turbopack: {},
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-// Import file CSS global yang baru saja Anda buat
 import '../styles/global.css';
 
-// Ini adalah komponen utama pembungkus aplikasi Next.js
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
